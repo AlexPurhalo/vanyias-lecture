670a189f50f66445a397e9a895ca042a5fb7fbe0
AlexPurhalo <alexpurhalo@gmail.com>
2017-02-24 21:47:06 +0200

    script-file-refactoring

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,22 @@
-const gallery = document.getElementById('gallery');
-const avatar = document.querySelector('.avatar');
-const phone = document.querySelector('.phone');
-const email = document.querySelector('.email');
-const name = document.querySelector('.name');
-const friendsList = document.getElementById('friends');
-const button = document.querySelector('button');
-const input = document.querySelector('input');
-const replies = document.querySelector('.replies');
+// Constants declaration
+const gallery = document.getElementById('gallery'),
+    avatar = document.querySelector('.avatar'),
+    phone = document.querySelector('.phone'),
+    email = document.querySelector('.email'),
+    name = document.querySelector('.name'),
+    friendsList = document.getElementById('friends'),
+    button = document.querySelector('button'),
+    input = document.querySelector('input'),
+    replies = document.querySelector('.replies')
 
 // Markup elements
 button.addEventListener('click', addReply);
 
-input.addEventListener('keydown', (e) => {
-    if (e.which == 13) addReply();
-});
+input.addEventListener('keydown', (e) => e.which == 13 && addReply());
 
 function addReply() {
-    let text = input.value.trim();
-    let reply = document.createElement('p');
+    let text = input.value.trim(), reply = document.createElement('p');
+
     reply.classList.add('reply');
     reply.innerHTML = `<p>${text}</p>`;
     replies.appendChild(reply);
@@ -29,9 +28,7 @@ function get(url, callback) {
     let xhr = new XMLHttpRequest();
 
     xhr.onreadystatechange = () => {
-        if (xhr.status == 200 && xhr.readyState == 4) {
-            callback(JSON.parse(xhr.responseText))
-        }
+        (xhr.status == 200 && xhr.readyState == 4) && callback(JSON.parse(xhr.responseText))
     };
 
     xhr.open('GET', url, true);
